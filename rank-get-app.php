9565d31ed1d8856205a68ead039bb5baaa3edb10
reiya0120 <55514865+reiya0120@users.noreply.github.com>
2021-10-24 06:04:35 +0900

    cmp rank-get

--- rank-get-app.php
+++ rank-get-app.php
@@ -1,13 +1,18 @@
 <?php
-require_once '/path/to/sdk-dir/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+
+$hits = '20';//商品データの取得数
+$serchUrl = '/komatuyamenbox/xae-n33r/';
+$count = '1';
 
 $client = new RakutenRws_Client();
 // アプリID (デベロッパーID) をセットします
-$client->setApplicationId('YOUR_APPLICATION_ID');
+$client->setApplicationId('1012490835504662103');
 
 // 楽天市場商品検索API では operation として 'IchibaItemSearch' を指定してください。
 // ここでは例として keyword に、うどんを指定しています。
 $response = $client->execute('IchibaItemSearch', array(
+  'hits' => $hits,
   'keyword' => 'うどん'
 ));
 
@@ -15,10 +20,12 @@ $response = $client->execute('IchibaItemSearch', array(
 if ($response->isOk()) {
     // 配列アクセスで情報を取得することができます。
     echo $response['count']."件見つかりました。\n";
-
     // foreach で商品情報を順次取得することができます。
     foreach ($response as $item) {
-        echo $item['itemName']."\n";
+        if (strpos($item['itemUrl'],$serchUrl)) {
+          echo $item['itemName']."・・・".$count."\n";
+        }
+        $count++;
     }
 } else {
     // getMessage() でレスポンスメッセージを取得することができます
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -16,15 +16,10 @@ class ComposerStaticInit643ea9bfd6003b75e03c058dad820fb1
         ),
     );
 
-    public static $classMap = array (
-        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixesPsr0 = ComposerStaticInit643ea9bfd6003b75e03c058dad820fb1::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit643ea9bfd6003b75e03c058dad820fb1::$classMap;
 
         }, null, ClassLoader::class);
     }
